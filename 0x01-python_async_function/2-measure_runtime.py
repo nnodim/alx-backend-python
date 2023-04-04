33db201ede06af6f5f7d1f648b132e19310379fc
nnodim <nnodimobinna@gmail.com>
2023-04-04 01:27:36 +0000

    2. Measure the runtime

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -9,10 +9,10 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """Create a measure_time function with integers n and max_delay as arguments
+    """Create a measure_time function with integers n and max_delay as args
     """
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
+    end = time.time()
     total_time = end - start
     return (total_time / n)
