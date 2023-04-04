9e5e67dc3f6230a0bb0e8b138a9a07b6ce1febb6
nnodim <nnodimobinna@gmail.com>
2023-04-04 01:45:27 +0000

    4. Tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,7 +9,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''Return the list of all delays (in seconds) for the executed tasks.
+    '''Return the list of all delays (in seconds) 
+    for the executed tasks.
     '''
     tasks = [task_wait_random(max_delay) for i in range(n)]
     finished_tasks = []
