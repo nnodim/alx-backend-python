6ef2b63c19fe063734504d9788ed415c1243657d
nnodim <nnodimobinna@gmail.com>
2023-04-25 03:11:59 +0100

    3. Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -45,13 +45,38 @@ class TestGetJson(unittest.TestCase):
             ('http://holberton.io', {'payload': False})
         ]
     )
-    def test_get_json(self, url, expected_result):
+    def test_get_json(self, url, test_payload):
         """A method to test that get_json returns the expected result."""
         mock_get = Mock()
-        mock_get.json.return_value = expected_result
+        mock_get.json.return_value = test_payload
         with patch('requests.get', return_value=mock_get):
             response = get_json(url)
-            self.assertEqual(response, expected_result)
+            self.assertEqual(response, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """
+    A class with a method and a memoized property
+    """
+
+    def test_memoize(self):
+        """Test memoize"""
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        test_instance = TestClass()
+        with patch.object(TestClass, 'a_method') as mock:
+            mock.return_value = 42
+
+            result1 = test_instance.a_property
+            result2 = test_instance.a_property
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            mock.assert_called_once()
 
 
 if __name__ == "__main__":
